5a1db4efef20abc4f65c3fffd460feaad321a03c
Vinod663 <vinodfernando048@gmail.com>
2025-03-25 16:10:59 +0530

    Update Login

--- src/main/java/org/example/controller/LoginController.java
+++ src/main/java/org/example/controller/LoginController.java
@@ -1,6 +1,5 @@
 package org.example.controller;
 
-import at.favre.lib.crypto.bcrypt.BCrypt;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -9,39 +8,52 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.util.Duration;
 import org.example.bo.BOFactory;
 import org.example.bo.custom.BOTypes;
 import org.example.bo.custom.LoginBO;
-import org.example.config.FactoryConfiguration;
-
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 
 import java.io.IOException;
-import java.util.List;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
 
     LoginBO loginBO = (LoginBO) BOFactory.getInstance().getBO(BOTypes.LOGIN);
     private String role;
-
-    @FXML
-    private Button LoginBtn;
-
-    @FXML
-    private PasswordField PswField;
-
-    @FXML
-    private TextField UsernameTextId;
+    private boolean isPasswordVisible = false;
 
     @FXML
     private AnchorPane ancLogin;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button passwordVisibilityButton;
+
+    @FXML
+    private HBox passwordHBox;
+
+    @FXML
+    private ImageView passwordVisibilityIcon;
+
+    @FXML
+    private TextField showPasswordTextField;
+
+    @FXML
+    private TextField usernameTextField;
 
     @FXML
     void LoginAction(ActionEvent event) throws IOException {
@@ -72,9 +84,9 @@ public class LoginController {
             System.out.println("Login failed");
             *//*e.printStackTrace();*//*
         }*/
-        boolean authenticate = loginBO.authenticate(UsernameTextId.getText(), PswField.getText());
+        boolean authenticate = loginBO.authenticate(usernameTextField.getText(), passwordField.getText());
         if (authenticate) {
-           /* System.out.println("Login successful!");*/
+            /* System.out.println("Login successful!");*/
             role = loginBO.getRole();
             System.out.println("role-"+role);
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/MainLayout.fxml"));
@@ -102,14 +114,59 @@ public class LoginController {
         } else {
             boolean wrongPsw = loginBO.isWrongPsw();
             if(wrongPsw) {
-                UsernameTextId.setStyle("-fx-text-box-border: green; -fx-text-inner-color: green;");
-                PswField.setStyle("-fx-text-box-border: red; -fx-text-inner-color: red;");
+                usernameTextField.setStyle("-fx-text-box-border: green; -fx-text-inner-color: green;");
+                passwordHBox.setStyle("-fx-border-color: red; -fx-text-inner-color: red; -fx-border-radius: 5px;");
             } else {
-                UsernameTextId.setStyle("-fx-text-box-border: red; -fx-text-inner-color: red;");
-                PswField.setStyle("-fx-text-box-border: red; -fx-text-inner-color: red;");
+                usernameTextField.setStyle("-fx-text-box-border: red; -fx-text-inner-color: red;");
+                passwordHBox.setStyle("-fx-border-color: red; -fx-text-inner-color: red; -fx-border-radius: 5px;");
             }
             System.out.println("Try again");
         }
     }
 
+
+    @FXML
+    void togglePasswordVisibility(ActionEvent event) {
+        isPasswordVisible = !isPasswordVisible;
+
+        try {
+            if (isPasswordVisible) {
+                // Show password
+                showPasswordTextField.setVisible(true);
+                showPasswordTextField.setManaged(true);
+                passwordField.setVisible(false);
+                passwordField.setManaged(false);
+
+                // Load image with full path checking
+                URL visibilityOnURL = getClass().getResource("/assets/icons/icons8-visibility-off-30.png");
+                if (visibilityOnURL != null) {
+                    passwordVisibilityIcon.setImage(new Image(visibilityOnURL.toString()));
+                } else {
+                    System.err.println("Visibility on icon not found");
+                }
+            } else {
+                // Hide password
+                showPasswordTextField.setVisible(false);
+                showPasswordTextField.setManaged(false);
+                passwordField.setVisible(true);
+                passwordField.setManaged(true);
+
+                // Load image with full path checking
+                URL visibilityOffURL = getClass().getResource("/assets/icons/icons8-visibility-30.png");
+                if (visibilityOffURL != null) {
+                    passwordVisibilityIcon.setImage(new Image(visibilityOffURL.toString()));
+                } else {
+                    System.err.println("Visibility off icon not found");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Bind text properties to sync password fields
+        showPasswordTextField.textProperty().bindBidirectional(passwordField.textProperty());
+    }
 }
