09e1097a712d6bc3985df5e9cba098a9d81e4971
Vinod663 <vinodfernando048@gmail.com>
2025-04-26 18:51:38 +0530

    Financial Report Update

---
+++ src/main/java/org/example/controller/FinancialReportController.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class FinancialReportController {
+    @FXML
+    private void onApply(ActionEvent event) {}
+
+    @FXML
+    private void onExport(ActionEvent event) {}
+
+    @FXML
+    private void onPrint(ActionEvent event) {}
+
+    @FXML
+    private void onPrev(ActionEvent event) {}
+
+    @FXML
+    private void onNext(ActionEvent event) {}
+
+}
